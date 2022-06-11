c5e6772602da7aaf0c395dfef3e11324c9d5db67
Bradley Grover <ac111897@avcol.school.nz>
2022-06-11 20:48:50 +1200

    fully change to web project so it can run on heroku lol

--- LotusMusic.App/Program.cs
+++ LotusMusic.App/Program.cs
@@ -1,11 +1,55 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Discord;
+using Discord.Commands;
+using Microsoft.Extensions.Hosting;
+using Discord.Addons.Hosting;
 
 namespace LotusMusic.App;
 
 public class Program
 {
-    public static async Task Main()
+    public static void Main(string[] args)
     {
-        await Startup.CreateHostBuilder().Build().RunAsync();
+        CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(logging =>
+            {
+                logging.AddConsole();
+                logging.SetMinimumLevel(LogLevel.Debug);
+            })
+            .ConfigureDiscordHost((context, config) =>
+            {
+                config.SocketConfig = new()
+                {
+                    LogLevel = LogSeverity.Debug,
+                    AlwaysDownloadUsers = false,
+                    MessageCacheSize = 0XC8
+                };
+                config.Token = GetToken(context.Configuration);
+            })
+            .UseCommandService((context, config) =>
+            {
+                config.CaseSensitiveCommands = false;
+                config.LogLevel = LogSeverity.Debug;
+                config.DefaultRunMode = RunMode.Async;
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseDefaultServiceProvider(x =>
+                {
+                    x.ValidateOnBuild = true;
+                });
+            });
+
+
+    private static string GetToken(IConfiguration configuration)
+    {
+        if (configuration.GetSection("Client-Configuration").GetValue<bool>("Use-Test-Token"))
+        {
+            return Environment.GetEnvironmentVariable("DISCORD_TOKEN_DEBUG") ?? throw new InvalidOperationException("No debug token set");
+        }
+        return Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? throw new InvalidOperationException("No discord bot token is set");
     }
 }
---
+++ LotusMusic.App/StartupExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using LotusMusic.App.Services;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Victoria;
+using LotusMusic.Core.Local;
+using LotusMusic.Core.Paging;
+using Microsoft.AspNetCore.Builder;
+using Discord.WebSocket;
+
+namespace LotusMusic.App;
+
+internal static class StartupExtensions
+{
+
+    internal static void AddHostedServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+#if !DEBUG
+            AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
+            options.UseSqlite(context.Configuration.GetConnectionString("Default"));
+#else
+            options.UseInMemoryDatabase($"Debug");
+#endif
+
+        });
+        services.AddHostedService<CommandHandler>();
+        services.AddHostedService<StatusHandler>();
+    }
+    internal static void AddNetworkServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddLavaNode(x =>
+        {
+            x.SelfDeaf = false;
+            x.Hostname = "lava.link";
+            x.Port = 80;
+        });
+    }
+
+    internal static void AddSingletons(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<ILocalSource, LocalSource>();
+        services.AddSingleton<IAudioPlayer, LavalinkAudio>();
+        services.AddSingleton<IPageResolver, PageResolver>();
+    }
+}
